6c7d24abad3a84ffd0499b9f71b52c06c1b8743c
root <root@slattaker.localdomain>
2018-06-10 10:18:31 -0400

    final project commit

--- jarvis-cli-web/js/web-pages/hulk-detail-page.js
+++ jarvis-cli-web/js/web-pages/hulk-detail-page.js
@@ -1,11 +1,22 @@
 var Menu = require('../web-components/menu-component')
 var getAvengers = require('../web-components/avengers-component')
-var HulkDetailPage = function(){
+var HulkDetailPage = function() {
 
- Menu()
+  Menu()
+  var content = document.getElementById('content')
+var button = document.createElement('button')
+button.onclick = getAvengers
+button.innerHTML = 'Get Avengers'
 
- var content = document.getElementById('content')
- content.innerHTML = 'This is the Hulks detail page'
-}
+var card = document.createElementById('div')
+card.className'w3-card-4'
+var img = document.createElement('img')
+img.src = '../app/assets/littlehulk.png'
+card.append(img)
 
+
+content.append(h1)
+content.append(button)}
 module.exports = HulkDetailPage
+
+
